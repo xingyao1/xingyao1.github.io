0f851b8f16c2ef5d1f3b104caa3300a0a60d96e6
xingyao1 <107181901+xingyao1@users.noreply.github.com>
2024-01-03 21:09:46 +0800

    Add files via upload

---
+++ js/登录页面切换.js
@@ -0,0 +1,35 @@
+    // // 登录注册页面切换
+    // let denglu1 = document.querySelector('.biaodan');
+    // let zhuce1 = document.querySelector('.biaodant');
+   
+    // console.log( denglu1.children[1].children[3]);
+    // denglu1.children[1].children[3].onclick =function(){
+    //     denglu1.style.display = 'none';
+    //     zhuce1.style.display = 'block';  
+    //     xianshi.innerHTML = '';        
+    // }
+    // zhuce1.children[1].children[3].onclick = function(){
+    //     zhuce1.style.display = 'none';
+    //     denglu1.style.display = 'block';
+    //     xianshi2.innerHTML = '';
+    // }
+    // // 登录注册页面切换结束
+//   ------------------------------
+    // 登录注册页面切换
+    let denglu1 = document.querySelector('.biaodan');
+    let zhuce1 = document.querySelector('.biaodant');
+   
+    console.log( denglu1.children[1].children[3]);
+    denglu1.children[1].children[3].onclick =function(){
+        denglu1.style.display = 'none';
+        zhuce1.style.display = 'block';  
+        xianshi.innerHTML = '';        
+    }
+    zhuce1.children[1].children[3].onclick = function(){
+        // 注册返回登录刷新页面
+        location.reload();
+    }
+    // 登录注册页面切换结束
+  
+
+  
